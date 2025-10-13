578bc3192405c8019d94bd01cc3edddcf58cfe5a
= <=>
2025-10-13 09:49:19 +0200

    fix: deleted empty do_install.sh file, fixed syntax in gco

--- shortcuts/gco.sh
+++ shortcuts/gco.sh
@@ -1,6 +1,6 @@
 branch_exists=$(git rev-parse --abbrev-ref $1 2>/dev/null)
 if [[ $? == 0 ]]; then
-echo $1 exists ($branch_exists), checking it out
+echo $1 exists: $branch_exists, checking it out
 git checkout $1
 else
 echo branch $1 does not exist, creating it
